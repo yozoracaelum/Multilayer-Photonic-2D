9e5b7e1c629c0ac19f8c588cc7cb7ff6dcefa0f8
Julian Evan <86104516+yozoracaelum@users.noreply.github.com>
2022-10-17 08:31:50 +0700

    Update Project.py

--- Project.py
+++ Project.py
@@ -7,35 +7,38 @@ n2 = float(input("n2: "))
 d = float(input("d(\u03BCm): "))
 N = int(input("N: "))
 d1,d2 = d,d
-m0,m1,m11,m00,m10,m10,m110,Mt = [],[],[],[],[],[],[],[]
-lamda = np.arange(0.1,2.1,0.01)
-w = [(2*np.pi)/i for i in lamda]
-k0 = [i*n0 for i in w]
-k1 = [i*n1 for i in w]
-k2 = [i*n2 for i in w]
-M11 = [np.exp(1j*k1[i]*d1)*(np.cos(k2[i]*d2)+0.5j*((k2[i]/k1[i])+(k1[i]/k2[i]))*np.sin(k2[i]*d2)) for i in range(len(lamda))]
-M12 = [np.exp(-1j*k1[i]*d1)*(0.5j*((k2[i]/k1[i])-(k1[i]/k2[i]))*np.sin(k2[i]*d2)) for i in range(len(lamda))]
-M21 = [np.exp(1j*k1[i]*d1)*(-0.5j*((k2[i]/k1[i])-(k1[i]/k2[i]))*np.sin(k2[i]*d2)) for i in range(len(lamda))]
-M22 = [np.exp(-1j*k1[i]*d1)*(np.cos(k2[i]*d2)-0.5j*((k2[i]/k1[i])+(k1[i]/k2[i]))*np.sin(k2[i]*d2)) for i in range(len(lamda))]
-for i in range(len(lamda)):
-    m00.append(M11[i])
-    m00.append(M12[i])
-    m0.append(m00)
-    m10.append(M21[i])
-    m10.append(M22[i])
-    m1.append(m10)
-    m110.append(m0[i])
-    m110.append(m1[i])
-    m11.append(m110)
-    m00,m10,m110 = [],[],[]
-m11 = np.array(m11)
-A = m11[0]
-for i in range(len(lamda)):
-    for j in range(N-1):
-        A = A@m11[i]
-    Mt.append(A)
-Mt = np.array(Mt)
+def TMM(n0,n1,n2,d1,d2,N):
+    m0,m1,m11,m00,m10,m10,m110,Mt = [],[],[],[],[],[],[],[]
+    lamda = np.arange(0.1,2.1,0.01)
+    w = [(2*np.pi)/i for i in lamda]
+    k0 = [i*n0 for i in w]
+    k1 = [i*n1 for i in w]
+    k2 = [i*n2 for i in w]
+    M11 = [np.exp(1j*k1[i]*d1)*(np.cos(k2[i]*d2)+0.5j*((k2[i]/k1[i])+(k1[i]/k2[i]))*np.sin(k2[i]*d2)) for i in range(len(lamda))]
+    M12 = [np.exp(-1j*k1[i]*d1)*(0.5j*((k2[i]/k1[i])-(k1[i]/k2[i]))*np.sin(k2[i]*d2)) for i in range(len(lamda))]
+    M21 = [np.exp(1j*k1[i]*d1)*(-0.5j*((k2[i]/k1[i])-(k1[i]/k2[i]))*np.sin(k2[i]*d2)) for i in range(len(lamda))]
+    M22 = [np.exp(-1j*k1[i]*d1)*(np.cos(k2[i]*d2)-0.5j*((k2[i]/k1[i])+(k1[i]/k2[i]))*np.sin(k2[i]*d2)) for i in range(len(lamda))]
+    for i in range(len(lamda)):
+        m00.append(M11[i])
+        m00.append(M12[i])
+        m0.append(m00)
+        m10.append(M21[i])
+        m10.append(M22[i])
+        m1.append(m10)
+        m110.append(m0[i])
+        m110.append(m1[i])
+        m11.append(m110)
+        m00,m10,m110 = [],[],[]
+    m11 = np.array(m11)
+    A = m11[0]
+    for i in range(len(lamda)):
+        for j in range(N-1):
+            A = A@m11[i]
+        Mt.append(A)
+    Mt = np.array(Mt)
+    return Mt,lamda
 #print(Mt)
+Mt,lamda = TMM(n0,n1,n2,d1,d2,N)
 print(Mt[0][1][0])
 r = [-(Mt[i][1][0]/Mt[i][1][1]) for i in range(len(lamda))]
 t = [Mt[i][0][0]-((Mt[i][0][1]*Mt[i][1][0])/Mt[i][1][1]) for i in range(len(lamda))]
